cf2f86a9035d974556f7e8b1e34166b6357d7768
Sloane Perrault <sloane@perrault.email>
2023-07-24 09:54:28 -0400

    clean up some things, remove opinions from template

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -3,13 +3,20 @@ import esbuild from "esbuild";
 import process from "process";
 import builtins from "builtin-modules";
 
-const copyToTestVault = () => {
-	const testVaultPluginDir =
-		"obsidianrc-test-vault/.obsidian/plugins/obsidianrc";
-	fs.copyFileSync("main.js", `${testVaultPluginDir}/main.js`);
-	fs.copyFileSync("styles.css", `${testVaultPluginDir}/styles.css`);
-	fs.copyFileSync("manifest.json", `${testVaultPluginDir}/manifest.json`);
-	console.log(`Copied build to ${testVaultPluginDir}`);
+const saveToTestVault = {
+  name: "save-to-test-vault",
+  setup(build) {
+    build.onEnd((result) => {
+      if (result.errors.length === 0) {
+        const testVaultPluginDir =
+          "obsidianrc-test-vault/.obsidian/plugins/obsidianrc";
+        fs.copyFileSync("main.js", `${testVaultPluginDir}/main.js`);
+        fs.copyFileSync("styles.css", `${testVaultPluginDir}/styles.css`);
+        fs.copyFileSync("manifest.json", `${testVaultPluginDir}/manifest.json`);
+        console.log(`Copied build to ${testVaultPluginDir}`);
+      }
+    });
+  },
 };
 
 const banner = `/*
@@ -21,52 +28,39 @@ if you want to view the source, please visit the github repository of this plugi
 const prod = process.argv[2] === "production";
 
 const context = await esbuild.context({
-	banner: {
-		js: banner,
-	},
-	entryPoints: ["main.ts"],
-	bundle: true,
-	external: [
-		"obsidian",
-		"electron",
-		"@codemirror/autocomplete",
-		"@codemirror/collab",
-		"@codemirror/commands",
-		"@codemirror/language",
-		"@codemirror/lint",
-		"@codemirror/search",
-		"@codemirror/state",
-		"@codemirror/view",
-		"@lezer/common",
-		"@lezer/highlight",
-		"@lezer/lr",
-		...builtins,
-	],
-	format: "cjs",
-	target: "es2018",
-	logLevel: "info",
-	sourcemap: prod ? false : "inline",
-	treeShaking: true,
-	outfile: "main.js",
-	plugins: prod
-		? []
-		: [
-				{
-					name: "save-to-test-vault",
-					setup(build) {
-						build.onEnd((result) => {
-							if (result.errors.length === 0) {
-								copyToTestVault();
-							}
-						});
-					},
-				},
-		  ],
+  banner: {
+    js: banner,
+  },
+  entryPoints: ["main.ts"],
+  bundle: true,
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+    ...builtins,
+  ],
+  format: "cjs",
+  target: "es2018",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "main.js",
+  plugins: prod ? [] : [saveToTestVault],
 });
 
 if (prod) {
-	await context.rebuild();
-	process.exit(0);
+  await context.rebuild();
+  process.exit(0);
 } else {
-	await context.watch();
+  await context.watch();
 }
